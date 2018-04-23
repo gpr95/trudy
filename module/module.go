4c778755f7cb24236b2546732978864003b52e78
gpr95 <gprzytula95@gmail.com>
2018-04-23 15:29:38 -0700

    Trudy zero checksum try more

--- module/module.go
+++ module/module.go
@@ -29,8 +29,10 @@ func (input Data) DoMangle() bool {
 //Mangle can modify/replace the Bytes values within the Data struct. This can
 //be empty if no programmatic mangling needs to be done.
 func (input *Data) Mangle() {
-		input.Bytes[40] = 0x00
-		input.Bytes[41] = 0x00
+		if input.Bytes[47] & 0x10 {
+			input.Bytes[50] = 0x00
+			input.Bytes[51] = 0x00
+		}
 }
 
 //Drop will return true if the Data needs to be dropped before going through
